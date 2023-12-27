ab301026ed6b503049614569dc0aa6ad477b3f52
Aditya Singh99g <Adityasinghdacs@gmail.com>
2023-12-27 18:34:14 +0530

    all files are added

---
+++ app/src/main/java/com/example/servicestutorial/MusicService.java
@@ -0,0 +1,39 @@
+package com.example.servicestutorial;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mediaPlayer =MediaPlayer.create(this, Settings.System.DEFAULT_ALARM_ALERT_URI);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public void onDestroy() {
+
+        mediaPlayer.stop();
+
+        super.onDestroy();
+    }
+}
