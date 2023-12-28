d80f7a14238ce55297c9d528ba59d8f8444ebdfa
Aditya Singh99g <Adityasinghdacs@gmail.com>
2023-12-28 21:30:33 +0530

    understand handler,... and prblm is cant know differnet thread name after using it

--- app/src/main/java/com/example/servicestutorial/MainActivity.java
+++ app/src/main/java/com/example/servicestutorial/MainActivity.java
@@ -6,13 +6,22 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.SynchronousQueue;
+
+import kotlin.jvm.Synchronized;
 
 public class MainActivity extends AppCompatActivity {
 
     Button BtnStart, BtnTerminate;
+    Runnable runnable;
+    String Tag = "laura";
 
 
     @Override
@@ -37,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 stopService(new Intent(MainActivity.this, MusicService.class));
+                Toast.makeText(MainActivity.this, "Terminate Btn Thread name: " + Thread.currentThread().getName(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -47,18 +57,83 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //using direct EditText without creating specific Variable because it is only used one time.
-                int time =Integer.parseInt(((EditText)(findViewById(R.id.EdtTime))).getText().toString());
+                int time = Integer.parseInt(((EditText) (findViewById(R.id.EdtTime))).getText().toString());
 
-                long triggerTime = System.currentTimeMillis()+(time*1000);
+                long triggerTime = System.currentTimeMillis() + (time * 1000);
 
-                Intent iBrodcast = new Intent(MainActivity.this,MyReceiver.class);
+                Intent iBrodcast = new Intent(MainActivity.this, MyReceiver.class);
 
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,100,iBrodcast,PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 100, iBrodcast, PendingIntent.FLAG_UPDATE_CURRENT);
 
-                alarmManager.set(AlarmManager.RTC_WAKEUP,triggerTime,pendingIntent);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent);
 
             }
         });
+
+
+        runnable = new Runnable() {
+            final Handler handler = new Handler();
+
+            @Override
+            public void run() {
+                synchronized (this) {
+                    try {
+                        wait(10000);
+
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+
+//                   new Handler().post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                          //  Toast.makeText(MainActivity.this, "Name of new Thread is : " + Thread.currentThread().getName(), Toast.LENGTH_SHORT).show();
+//                            Toast.makeText(MainActivity.this, "Name of new Thread is : " + Thread.currentThread().getName(), Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    });
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //  Toast.makeText(MainActivity.this, "Name of new Thread is : " + Thread.currentThread().getName(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Name of new Thread is -> " + Thread.currentThread().getName(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        // note app will not  run after 10sec  becoz dusre thread se dusre thread ko access kar rhe ho
+        thread.start();
+
+
+//        Handler handler1 = new Handler();
+//       new Handler().post(new Runnable() {
+//            @Override
+//            public void run() {
+//                synchronized (this){
+//                    try {
+//                        wait(10000);
+//                    } catch (Exception e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                }
+//            }
+//        });
+//
+
+
+//        synchronized (this){
+//            try {
+//                wait(10000);
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
 
     }
